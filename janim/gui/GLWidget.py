9978368f2176fe3c24ee6bd95d5e5ab684fb5441
jkjkil4 <1173374788@qq.com>
2023-07-07 08:26:16 +0800

    移除部分测试代码

--- janim/gui/GLWidget.py
+++ janim/gui/GLWidget.py
@@ -43,42 +43,6 @@ class GLWidget(QOpenGLWidget):
         # glClearColor(0, 0, 0, 1)    # 将背景色设置为黑色
         glEnable(GL_MULTISAMPLE)    # 抗锯齿
 
-        # self.shader = QOpenGLShaderProgram()
-        # self.shader.addShaderFromSourceFile(QOpenGLShader.ShaderTypeBit.Vertex, 'shaders/tex.vert')
-        # self.shader.addShaderFromSourceFile(QOpenGLShader.ShaderTypeBit.Fragment, 'shaders/tex.frag')
-        # if not self.shader.link():
-        #     print('Failed to link GLWidget.shader')
-        #     exit(1)
-        # self.shader.bind()
-        
-        # vertices = np.array([
-        #     # positions       # texture coords
-        #     0.5,  0.5, 0.0,   1.0, 1.0, # top right
-        #     0.5, -0.5, 0.0,   1.0, 0.0, # bottom right
-        #    -0.5, -0.5, 0.0,   0.0, 0.0, # bottom left
-        #    -0.5,  0.5, 0.0,   0.0, 1.0  # top left 
-        # ]).astype(np.float32)
-
-        # self.indices = np.array([
-        #     0, 1, 3,
-        #     1, 2, 3
-        # ]).astype(np.uint)
-
-        # self.vao = glGenVertexArrays(1)
-        # self.vbo = glGenBuffers(1)
-
-        # glBindVertexArray(self.vao)
-
-        # glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        # glBufferData(GL_ARRAY_BUFFER, len(vertices) * FLOAT_SIZE, vertices, GL_STATIC_DRAW)
-
-        # glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * FLOAT_SIZE, ctypes.c_void_p(0))
-        # glEnableVertexAttribArray(0)
-        # glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * FLOAT_SIZE, ctypes.c_void_p(3 * FLOAT_SIZE))
-        # glEnableVertexAttribArray(1)
-
-        # glBindVertexArray(0)
-
         # self.tex1 = QOpenGLTexture(QOpenGLTexture.Target.Target2D)
         # self.tex1.create()
         # self.tex1.setData(QImage('assets/container.jpg').mirrored(False, True))
@@ -106,22 +70,6 @@ class GLWidget(QOpenGLWidget):
         # self.tex2.bind()
 
         # self.shader.bind()
-
-        # model = QMatrix4x4(IDEN_MAT)
-        # model.rotate(60 * (time.time() % 6), 1, 0, 0)
-        # self.shader.setUniformValue(self.shader.uniformLocation('model'), model)
-
-        # view = QMatrix4x4(IDEN_MAT)
-        # view.translate(0, 0, -3)
-        # self.shader.setUniformValue(self.shader.uniformLocation('view'), view)
-
-        # projection = QMatrix4x4(IDEN_MAT)
-        # projection.perspective(45, self.width() / self.height(), 0.1, 100)
-        # self.shader.setUniformValue(self.shader.uniformLocation('projection'), projection)
-
-        # glBindVertexArray(self.vao)
-        # glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, self.indices)
-        # glBindVertexArray(0)
 
     def resizeGL(self, w: int, h: int) -> None:
         super().resizeGL(w, h)
