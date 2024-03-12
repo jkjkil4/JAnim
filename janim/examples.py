79515ef0c7b465fc673eef057a7e55e6fa084db2
jkjkil4 <1173374788@qq.com>
2024-03-12 19:00:50 +0800

    feat: AnimatingPiExample

--- janim/examples.py
+++ janim/examples.py
@@ -95,6 +95,36 @@ class TypstExample(Timeline):
         self.forward()
 
 
+class AnimatingPiExample(Timeline):
+    def construct(self) -> None:
+        grid = Typst('pi') * 100
+        grid.points.scale(2).arrange_in_grid(10, 10, buff=0.2)
+        grid.show()
+
+        self.play(grid.anim.points.shift(LEFT))
+        self.play(grid(VItem).anim.color.set(YELLOW))
+        self.forward()
+        self.play(grid(VItem).anim.color.set(BLUE))
+        self.forward()
+        self.play(grid.anim.points.to_center().set_height(TAU - MED_SMALL_BUFF))
+        self.forward()
+
+        self.play(grid.anim.points.apply_complex_fn(np.exp), duration=5)
+        self.forward()
+
+        self.play(
+            grid.anim.points.apply_point_fn(
+                lambda p: [
+                    p[0] + 0.5 * math.sin(p[1]),
+                    p[1] + 0.5 * math.sin(p[0]),
+                    p[2]
+                ]
+            ),
+            duration=5
+        )
+        self.forward()
+
+
 class UpdaterExample(Timeline):
     def construct(self) -> None:
         square = Square(fill_color=BLUE_E, fill_alpha=1).show()
