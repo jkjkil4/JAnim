f7686f96d3c31874104724ff4d1f9e444981b870
jkjkil4 <1173374788@qq.com>
2024-06-06 09:52:52 +0800

    feat: i18n for CLI

--- janim/__main__.py
+++ janim/__main__.py
@@ -1,33 +1,40 @@
 import os
 from argparse import ArgumentParser, Namespace
 
-from janim.locale.i18n import set_lang
+from janim.locale.i18n import get_local_strings, set_lang
 from janim.utils.file_ops import get_janim_dir
+
+_ = get_local_strings('__main__')
 
 
 def main() -> None:
-    parser = ArgumentParser(description='A library for simple animation effects')
-    parser.add_argument(
-        '--lang',
-        help='Language code, e.g., en, zh_CN'
-    )
-    initial_args = parser.parse_known_args()[0]
+    global _
+
+    initial_parser = ArgumentParser(add_help=False)
+    initial_parser.add_argument('--lang')
+    initial_args = initial_parser.parse_known_args()[0]
 
     if initial_args.lang:
         set_lang(initial_args.lang)
+        _ = get_local_strings('__main__')
 
+    parser = ArgumentParser(description=_('A library for simple animation effects'))
     parser.set_defaults(func=None)
 
+    parser.add_argument(
+        '--lang',
+        help=_('Language code, e.g., en, zh_CN')
+    )
     parser.add_argument(
         '-v', '--version',
         action='store_true'
     )
 
     sp = parser.add_subparsers()
-    run_parser(sp.add_parser('run', help='Run timeline(s) from specific namespace'))
-    write_parser(sp.add_parser('write', help='Generate video file(s) of timeline(s) from specific namesapce'))
-    examples_parser(sp.add_parser('examples', help='Show examples of janim'))
-    tool_parser(sp.add_parser('tool', help='Run useful tools'))
+    run_parser(sp.add_parser('run', help=_('Run timeline(s) from specific file')))
+    write_parser(sp.add_parser('write', help=_('Generate media file(s) of timeline(s) from specific file')))
+    examples_parser(sp.add_parser('examples', help=_('Show examples of JAnim')))
+    tool_parser(sp.add_parser('tool', help=_('Run useful tools')))
 
     args = parser.parse_args()
 
@@ -43,65 +50,79 @@ def main() -> None:
     args.func(args)
 
 
-def render_args(parser: ArgumentParser) -> None:
+def render_args(
+    parser: ArgumentParser,
+    timeline_names_help: str,
+    all_help: str
+) -> None:
     parser.add_argument(
         'filepath',
-        help='Path to file holding the python code for the timeline'
+        help=_('Path to file holding the Python code')
     )
     parser.add_argument(
         'timeline_names',
         nargs='*',
-        help='Name of the Timeline class you want to see'
+        help=timeline_names_help
     )
     parser.add_argument(
         '-a', '--all',
         action='store_true',
-        help='Render all timelines from a file'
+        help=all_help
     )
     parser.add_argument(
         '-c', '--config',
         nargs=2,
         metavar=('key', 'value'),
         action='append',
-        help='Override config'
+        help=_('Override config')
     )
 
 
 def run_parser(parser: ArgumentParser) -> None:
-    render_args(parser)
+    render_args(
+        parser,
+        _('Name of the Timeline class you want to preview'),
+        _('Preview all timelines from a file')
+    )
     parser.add_argument(
         '-i', '--interact',
         action='store_true',
-        help='Enable the network socket for interacting'
+        help=_('Enable the network socket for interacting with vscode')
     )
     parser.set_defaults(func=run)
 
 
 def write_parser(parser: ArgumentParser) -> None:
-    render_args(parser)
+    render_args(
+        parser,
+        _('Name of the Timeline class you want to write'),
+        _('Write all timelines from a file')
+    )
     parser.add_argument(
         '-o', '--open',
         action='store_true',
-        help='Open the video after writing'
+        help=_('Open the video after writing')
     )
     parser.add_argument(
         '--format',
         choices=['mp4', 'mov'],
         default='mp4',
-        help='Format of the output video'
+        help=_('Format of the output video')
     )
     parser.add_argument(
         '--audio_format',
         default='mp3',
-        help='Format of the output audio'
+        help=_('Format of the output audio')
     )
     parser.add_argument(
         '--video',
         action='store_true',
+        help=_('Video only')
     )
     parser.add_argument(
         '--audio',
-        action='store_true'
+        action='store_true',
+        help=_('Audio only')
     )
     parser.set_defaults(func=write)
 
@@ -111,7 +132,7 @@ def examples_parser(parser: ArgumentParser) -> None:
     parser.add_argument(
         'timeline_names',
         nargs='*',
-        help='Name of the example you want to see'
+        help=_('Name of the example you want to see')
     )
     parser.set_defaults(all=None)
     parser.set_defaults(config=None)
@@ -124,7 +145,7 @@ def tool_parser(parser: ArgumentParser) -> None:
         'tool_name',
         choices=['richtext', 'fonts'],
         nargs='*',
-        help='Tool(s) that you want to use'
+        help=_('Tool(s) that you want to use')
     )
     parser.set_defaults(func=tool)
 
