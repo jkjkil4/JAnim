660559118ab1528e01e5eda8daddfa6eb514d841
jkjkil4 <1173374788@qq.com>
2025-04-03 22:23:57 +0800

    refactor: test_examples

--- test/examples/test_examples.py
+++ test/examples/test_examples.py
@@ -1,10 +1,11 @@
 import os
-import subprocess as sp
+import shutil
 import sys
 import unittest
 from typing import Generator
 
 import numpy as np
+from PIL import Image
 
 import janim.examples as examples
 from janim.anims.timeline import Timeline
@@ -36,60 +37,58 @@ def get_timelines_for_test() -> list[type[Timeline]]:
 
 def load_tests(loader, standard_tests, pattern) -> unittest.TestSuite:
     class ExampleTester(unittest.TestCase):
-        def __init__(self, timeline_cls: type[Timeline], ref_path: str):
+        def __init__(self, timeline_cls: type[Timeline]):
             super().__init__('test')
             self.timeline_cls = timeline_cls
-            self.ref_path = ref_path
 
         def __str__(self) -> str:
             return f"{self.timeline_cls.__name__} ({self.__class__.__name__})"
 
         def test(self) -> None:
-            temp_file = os.path.join(Config.get.temp_dir, 'test_example.mov')
-
             with Config(pixel_width=WIDTH,
                         pixel_height=HEIGHT,
                         fps=5,
                         temp_dir='test/__test_tempdir__'):
-                anim = self.timeline_cls().build(quiet=True)
-                VideoWriter.writes(anim, temp_file, quiet=True)
+                built = self.timeline_cls().build(quiet=True)
 
-            render_frames = self.get_frames(temp_file)
-            ref_frames = self.get_frames(self.ref_path)
+            fps = built.cfg.fps
 
             max_errors = 0
             frame_number = -1
 
-            for i, (buf1, buf2) in enumerate(zip(render_frames, ref_frames, strict=True)):
-                render_data = np.frombuffer(buf1, dtype=np.uint8).astype(np.int16)
-                ref_data = np.frombuffer(buf2, dtype=np.uint8).astype(np.int16)
+            for frame, ref in zip(range(round(built.duration * fps) + 1),
+                                self.get_ref(self.timeline_cls.__name__)):
+                render_data = np.frombuffer(built.capture(frame / fps).tobytes(), dtype=np.uint8).astype(np.int16)
+                ref_data = np.frombuffer(ref, dtype=np.uint8).astype(np.int16)
                 delta = np.abs(render_data - ref_data)
-                errors = delta.sum()
+                errors = np.max(delta.reshape((-1, 4)).sum(axis=1))
                 if errors > max_errors:
                     max_errors = errors
-                    frame_number = i
+                    frame_number = frame
 
-            self.assertEqual(max_errors, 0, f'max_errors: {max_errors}, t: {frame_number / 5}')
+            if max_errors != 0:
+                guarantee_existence('test/__test_errors__')
+                built.capture(frame_number / fps).save(f'test/__test_errors__/{self.timeline_cls.__name__}_{frame_number}_err.png')
+            self.assertEqual(max_errors, 0, f'max_errors: {max_errors}, t: {frame_number / fps}')
 
         @staticmethod
-        def get_frames(video_path: str) -> Generator[bytes, None, None]:
-            ffmpeg_command = [
-                'ffmpeg',
-                '-i', video_path,
-                '-f', 'rawvideo',
-                '-pix_fmt', 'rgba',
-                '-'
-            ]
-            process = sp.Popen(ffmpeg_command, stdout=sp.PIPE, stderr=sp.PIPE)
+        def get_ref(timeline_name: str) -> Generator[bytes, None, None]:
+            frame = 0
+            ref = None
+            while True:
+                ref_path = os.path.join(ref_dir, f'{timeline_name}_{frame}.png')
+                if os.path.exists(ref_path):
+                    ref = Image.open(ref_path).tobytes()
+                assert ref is not None
+                frame += 1
+                yield ref
 
-            while raw_frame := process.stdout.read(HEIGHT * WIDTH * 4):
-                yield raw_frame
 
-    path = get_ref_dir()
+    ref_dir = get_ref_dir()
     timelines = get_timelines_for_test()
     suite = unittest.TestSuite()
     suite.addTests([
-        ExampleTester(timeline, os.path.join(path, f'{timeline.__name__}.mov'))
+        ExampleTester(timeline)
         for timeline in timelines
     ])
     return suite
@@ -101,13 +100,20 @@ def generate_ref() -> None:
 
     with Config(pixel_width=WIDTH,
                 pixel_height=HEIGHT,
-                fps=5,
-                output_dir=output_dir):
+                fps=5):
         for timeline in get_timelines_for_test():
             if includes and timeline.__name__ not in includes:
                 continue
-            VideoWriter.writes(timeline().build(),
-                               os.path.join(output_dir, f'{timeline.__name__}.mov'))
+            built = timeline().build()
+            fps = built.cfg.fps
+            prev_bytes = None
+            for frame in range(round(built.duration * fps) + 1):
+                current = built.capture(frame / fps)
+                current_bytes = current.tobytes()
+                if prev_bytes is not None and prev_bytes == current_bytes:
+                    continue
+                current.save(os.path.join(output_dir, f'{timeline.__name__}_{frame}.png'))
+                prev_bytes = current_bytes
 
 
 if __name__ == '__main__':
