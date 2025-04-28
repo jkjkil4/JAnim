f16a25bc8278eb49199ccdfd14b4af3761cf1645
jkjkil4 <1173374788@qq.com>
2025-04-28 18:12:16 +0800

    test: update test_space_ops.py

--- test/utils/test_space_ops.py
+++ test/utils/test_space_ops.py
@@ -9,20 +9,20 @@ from janim.utils.space_ops import (angle_axis_from_quaternion,
 class TestQuaternionOperations(unittest.TestCase):
     def test_quaternion_from_angle_axis(self):
         angle = 1.0
-        axis = [1.0, 0.0, 0.0]
+        axis = [1.0, 0.5, 0.0]
         q = quaternion_from_angle_axis(angle, axis)
-        self.assertAlmostEqual(q[0], 0.479425538604203, places=6)
-        self.assertAlmostEqual(q[1], 0.0, places=6)
+        self.assertAlmostEqual(q[0], 0.428811237787379, places=6)
+        self.assertAlmostEqual(q[1], 0.2144056188936895, places=6)
         self.assertAlmostEqual(q[2], 0.0, places=6)
         self.assertAlmostEqual(q[3], 0.8775825618903728, places=6)
 
     def test_angle_axis_from_quaternion(self):
-        q = [0.5403023058681398, 0.8414709848078965, 0.0, 0.0]
+        q = [0.5403023058681398, 0.8414709848078965, 0.5, 0.0]
         angle, axis = angle_axis_from_quaternion(q)
         self.assertAlmostEqual(angle, 3.141592653589793, places=6)
-        self.assertAlmostEqual(axis[0], 0.5403023058681398, places=6)
-        self.assertAlmostEqual(axis[1], 0.8414709848078965, places=6)
-        self.assertAlmostEqual(axis[2], 0.0, places=6)
+        self.assertAlmostEqual(axis[0], 0.54030231, places=6)
+        self.assertAlmostEqual(axis[1], 0.84147098, places=6)
+        self.assertAlmostEqual(axis[2], 0.5, places=6)
 
     def test_rotate_vector(self):
         v = [1.0, 2.0, 3.0]
@@ -32,12 +32,12 @@ class TestQuaternionOperations(unittest.TestCase):
         self.assertAlmostEqual(rotated_v[2], -1.5359856, places=6)
 
     def test_rotation_matrix_from_quaternion(self):
-        q = [0.5403023058681398, 0.8414709848078965, 0.0, 0.0]
+        q = [0.5403023058681398, 0.8414709848078965, 0.5, 0.0]
         R = rotation_matrix_from_quaternion(q)
         expected_R = [
-            [-0.41614684, 0.90929743,  0.        ],
-            [ 0.90929743, 0.41614684,  0.        ],
-            [ 0.        , 0.        , -1.        ],
+            [-0.53291747,  0.72743794,  0.43224184],
+            [ 0.72743794,  0.13291747,  0.67317679],
+            [ 0.43224184,  0.67317679, -0.6       ],
         ]
         for i in range(3):
             for j in range(3):
@@ -45,12 +45,12 @@ class TestQuaternionOperations(unittest.TestCase):
 
     def test_rotation_matrix(self):
         angle = 1.0
-        axis = [1.0, 0.0, 0.0]
+        axis = [1.0, 0.5, 0.0]
         R = rotation_matrix(angle, axis)
         expected_R = [
-            [1.0, 0.0, 0.0],
-            [0.0, 0.5403023058681398, -0.8414709848078965],
-            [0.0, 0.8414709848078965, 0.5403023058681398]
+            [ 0.90806046, 0.18387908,  0.37631726],
+            [ 0.18387908, 0.63224184, -0.75263453],
+            [-0.37631726, 0.75263453,  0.54030231],
         ]
         for i in range(3):
             for j in range(3):
