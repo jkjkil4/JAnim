9f12b1090dd056d3a7981ae716b2986db63df139
jkjkil4 <1173374788@qq.com>
2025-09-17 21:29:22 +0800

    refactor: extract `get_subpath_attr`

--- janim/render/shaders/vitem.frag.glsl
+++ janim/render/shaders/vitem.frag.glsl
@@ -50,67 +50,7 @@ float get_radius(int idx) {
 }
 
 #include "../includes/blend_color.glsl"
-#include "../includes/bezier_sdf.glsl"
-
-float cross2d(vec2 a, vec2 b) {
-    return a.x * b.y - a.y * b.x;
-}
-
-void get_subpath_attr(
-    int start_idx,
-    out int end_idx,
-    out int idx,
-    out float d,
-    out float sgn
-) {
-    const int lim = (points.length() - 1) / 2 * 2;
-    end_idx = lim;
-    bool is_closed = get_isclosed(start_idx);
-
-    d = INFINITY;
-    sgn = 1.0;
-    for (int i = start_idx; i < lim; i += 2) {
-        vec2 B = get_point(i + 1);
-        if (isnan(B.x)) {
-            end_idx = i;
-            break;
-        }
-        vec2 A = get_point(i), C = get_point(i + 2);
-        if (A == B && B == C)
-            continue;
-
-        vec2 v1 = normalize(B - A);
-        vec2 v2 = normalize(C - B);
-        // REFACTOR: 使用更好的判断可近似为直线的方法
-        if (abs(cross2d(v1, v2)) < 1e-3 && dot(v1, v2) > 0.0) {
-            vec2 e = C - A;
-            vec2 w = v_coord - A;
-            vec2 b = w - e * clamp(dot(w, e) / dot(e, e), 0.0, 1.0);
-            float dist = length(b);
-            if (dist < d) {
-                d = dist;
-                idx = i;
-            }
-
-            if (is_closed) {
-                bvec3 cond = bvec3( v_coord.y >= A.y,
-                                    v_coord.y  < C.y,
-                                    e.x * w.y > e.y * w.x );
-                if(all(cond) || all(not(cond))) sgn = -sgn;
-            }
-        } else {
-            float dist = distance_bezier(A, B, C, v_coord);
-            if (dist < d) {
-                d = dist;
-                idx = i;
-            }
-
-            if (is_closed) {
-                sgn *= sign_bezier(A, B, C, v_coord);
-            }
-        }
-    }
-}
+#include "vitem_subpath_attr.glsl"
 
 // #define CONTROL_POINTS
 // #define POLYGON_LINES
@@ -144,7 +84,7 @@ void main()
     const int lim = (points.length() - 1) / 2 * 2;
 
     while (true) {
-        get_subpath_attr(start_idx, start_idx, idx, sp_d, sp_sgn);
+        get_subpath_attr(start_idx, lim, start_idx, idx, sp_d, sp_sgn);
         d = min(d, sp_d);
         sgn *= sp_sgn;
 
