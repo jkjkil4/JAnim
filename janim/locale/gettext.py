161030fb5b267d71551b4be8caa3fce7abf162ed
jkjkil4 <1173374788@qq.com>
2025-12-24 16:07:44 +0800

    chore: update i18n files

--- janim/locale/gettext.py
+++ janim/locale/gettext.py
@@ -14,10 +14,11 @@ def main() -> None:
         for file in files:
             if not file.endswith('.py'):
                 continue
-            if includes and not is_included(file):
-                continue
             source = os.path.relpath(os.path.join(root, file), os.path.join(get_janim_dir(), '..'))
-            module_name = source.removesuffix('.py').replace('/', '.').replace('\\', '.')
+            source = source.replace('\\', '/')
+            if includes and not is_included(source):
+                continue
+            module_name = source.removesuffix('.py').replace('/', '.')
             dist = os.path.join(get_janim_dir(), 'locale', 'source', module_name + '.pot')
             cmd = f'xgettext -o "{dist}" "{source}"'
             print(cmd)
