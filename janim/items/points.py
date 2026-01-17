0f9b4041509c6cc4c616eff33385efd02b89e8a5
jkjkil4 <1173374788@qq.com>
2026-01-17 22:40:39 +0800

    chore

--- janim/items/points.py
+++ janim/items/points.py
@@ -234,11 +234,12 @@ class NamedGroup[T](Group[T]):
         return self
 
     def remove(self, *items_or_names: T | str) -> Self:
-        # 仿照删除物件的过程，更新 _named_indices
         items = [
             self.by_name(item_or_name) if isinstance(item_or_name, str) else item_or_name
             for item_or_name in items_or_names
         ]
+
+        # 仿照删除物件的过程，更新 _named_indices
         for obj in items:
             # 被删除的一个物件的下标
             try:
