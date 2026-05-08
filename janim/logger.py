84ece3fc313f546a2ec4b288f7a314af7ec71b5e
jkjkil4 <1173374788@qq.com>
2026-05-08 14:58:07 +0800

    chore: disable logger datefmt localization

--- janim/logger.py
+++ janim/logger.py
@@ -22,7 +22,7 @@ log.propagate = False
 log.setLevel('INFO')
 
 if not log.hasHandlers():
-    fmt = logging.Formatter('%(message)s', datefmt='[%X]')
+    fmt = logging.Formatter('%(message)s', datefmt='[%H:%M:%S]')
     rich_handler = RichHandler(
         console=Console(stderr=True)  # RichHandler 默认输出到 stdout，需要手动改为 stderr
     )
