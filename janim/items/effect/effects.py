3a7ad878b0626683fc7d87ff3b4af5d217c3ab62
jkjkil4 <1173374788@qq.com>
2026-09-08 19:30:47 +0800

    docs: enhance docs for `effects.py`

--- janim/items/effect/effects.py
+++ janim/items/effect/effects.py
@@ -14,6 +14,26 @@ from janim.utils.config import Config
 
 
 class AlphaEffect(SimpleFrameEffect):
+    """
+    透明度效果，使用 ``.alpha.set(...)`` 更新不透明度值
+
+    示例：
+
+    .. code-block:: python
+
+        circles = Circle(fill_alpha=1) * 4
+        circles.show()
+        circles.points.arrange_in_grid(buff=-0.4)
+
+        effect = AlphaEffect(circles)
+
+        self.play(
+            effect.anim.alpha.set(0),
+            effect.anim.alpha.set(1),
+            lag_ratio=1,
+        )
+    """
+
     alpha = CmptInfo(Cmpt_Alpha[Self], 1.0)
 
     def __init__(
@@ -79,6 +99,60 @@ class Shadertoy(FrameEffect):
     .. note::
 
         如果该着色器代码中出现报错，会显示为 ``JA_SHADERTOY`` 中出现的
+
+    可以使用 :meth:`anim_update` 创建 ``iTime`` uniform 更新的动画，例如：
+
+    .. code-block:: python
+
+        # Originates from https://www.shadertoy.com/view/mtyGWy
+        shader = Shadertoy(
+            \"""
+            /* This animation is the material of my first youtube tutorial about creative
+            coding, which is a video in which I try to introduce programmers to GLSL
+            and to the wonderful world of shaders, while also trying to share my recent
+            passion for this community.
+                                                Video URL: https://youtu.be/f4s1h2YETNY
+            */
+
+            //https://iquilezles.org/articles/palettes/
+            vec3 palette( float t ) {
+                vec3 a = vec3(0.5, 0.5, 0.5);
+                vec3 b = vec3(0.5, 0.5, 0.5);
+                vec3 c = vec3(1.0, 1.0, 1.0);
+                vec3 d = vec3(0.263,0.416,0.557);
+
+                return a + b*cos( 6.28318*(c*t+d) );
+            }
+
+            //https://www.shadertoy.com/view/mtyGWy
+            void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+                vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
+                vec2 uv0 = uv;
+                vec3 finalColor = vec3(0.0);
+
+                for (float i = 0.0; i < 4.0; i++) {
+                    uv = fract(uv * 1.5) - 0.5;
+
+                    float d = length(uv) * exp(-length(uv0));
+
+                    vec3 col = palette(length(uv0) + i*.4 + iTime*.4);
+
+                    d = sin(d*8. + iTime)/8.;
+                    d = abs(d);
+
+                    d = pow(0.01 / d, 1.2);
+
+                    finalColor += col * d;
+                }
+
+                fragColor = vec4(finalColor, 1.0);
+            }
+            \"""
+        )
+
+        self.play(
+            shader.anim_update(duration=4),
+        )
     """
 
     def __init__(
@@ -104,7 +178,16 @@ class Shadertoy(FrameEffect):
             optional=True,
         )
 
+    def anim_update(self, **kwargs) -> DataUpdater:
+        """
+        创建 ``iTime`` uniform 更新的动画
+        """
+        return DataUpdater(self, self.updater, **kwargs)
+
     def create_updater(self, **kwargs) -> DataUpdater:
+        """
+        同 :meth:`anim_update` ，此为旧名称
+        """
         return DataUpdater(self, self.updater, **kwargs)
 
     @staticmethod
